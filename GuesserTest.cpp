a73ca5b3065bd377ad566b7f0aac108a35737fdc
Ultramas <Ultramaster123456@gmail.com>
2026-04-29 11:58:57 -0700

    submission II

--- GuesserTest.cpp
+++ GuesserTest.cpp
@@ -44,27 +44,27 @@ TEST(GuesserTest, stest)
  
 
 /*check if the secret counts the "lock" as a guess*/
-TEST(GuesserTest, xtest)
+TEST(GuesserTest, lxtest)
 {
   Guesser object("Secret"); /* to invoke distance, call object.distance*/
-  ASSERT_EQ(Guesser.distance, 3); /*try with a distance greater than 2; i.e.,3 */
+  ASSERT_EQ(3, distance(guess)); /*try with a distance greater than 2; i.e.,3 */
   cout << "remaining_tries: " << m_remaining << endl;
 }
  
 
 /*check if the secret counts the "lock" as a guess*/
-TEST(GuesserTest, stest)
+TEST(GuesserTest, tstest)
 {
   Guesser object("Secret");
-  ASSERT_EQ(Guesser.distance, 3); /*try with a distance greater than 2; i.e.,3 */
+  ASSERT_EQ(3, distance(guess)); /*try with a distance greater than 2; i.e.,3 */
   cout << "remaining_tries: " << m_remaining << endl;
 }
 
 /*check if a "correct" guess resets the number of remaining attempts*/
-TEST(GuesserTest, stest)
+TEST(GuesserTest, ptest)
 {
   Guesser object("Secret");
-  ASSERT_EQ(Guesser.distance, 0); /*try with a distance greater than 2; i.e.,3 */
+  ASSERT_EQ(0, distance(guess)); /*try with a distance greater than 2; i.e.,3 */
   cout << "remaining_tries: " << m_remaining << endl;
 }
 
@@ -73,32 +73,48 @@ TEST(GuesserTest, stest)
 TEST(GuesserTest, stest)
 {
   Guesser object("Secret");
-  ASSERT_EQ(Guesser.distance, 1); /*try with a distance greater than 2; i.e.,3 */
+  ASSERT_EQ(1, distance(guess)); /*try with a distance greater than 2; i.e.,3 */
   cout << "remaining_tries: " << m_remaining << endl;
   Guesser object("Secret");
-  ASSERT_EQ(Guesser.distance, 1); /*try with a distance greater than 2; i.e.,3 */
+  ASSERT_EQ(1, distance(guess)); /*try with a distance greater than 2; i.e.,3 */
   cout << "remaining_tries: " << m_remaining << endl;
   Guesser object("Secret");
-  ASSERT_EQ(Guesser.distance, 2); /*try with a distance greater than 2; i.e.,3 */
+  ASSERT_EQ(1, distance(guess)); /*try with a distance greater than 2; i.e.,3 */
   cout << "remaining_tries: " << m_remaining << endl;
 }
 
 
 /*check if a "correct" guess resets the number of remaining attempts even after a lock*/
-TEST(GuesserTest, stest)
+TEST(GuesserTest, ytest)
 {
   Guesser object("Secret");
-  ASSERT_EQ(Guesser.distance, 3); /*try with a distance greater than 2; i.e.,3 */
+  ASSERT_EQ(3, distance(guess)); /*try with a distance greater than 2; i.e.,3 */
   cout << "remaining_tries: " << m_remaining << endl;
   Guesser object("Secret");
-  ASSERT_EQ(Guesser.distance, 0); /*try with a distance greater than 2; i.e.,3 */
+  ASSERT_EQ(0, distance(guess)); /*try with a distance greater than 2; i.e.,3 */
   cout << "remaining_tries: " << m_remaining << endl;
 }
 
 /*check if capitalization matters*/
-TEST(GuesserTest, stest)
+TEST(GuesserTest, gtest)
 {
   Guesser object("Secret");
   ASSERT_EQ(toUpperCase(guess), 1); /*try capitalizing the guess*/
+  cout << "remaining_tries: " << m_remaining << "problem with bool Guesser::match(string guess)" << endl;
+}
+
+/*check that if the secret is 2 or less chars, if it counts as a close guess for any length guess*/
+TEST(GuesserTest, gdtest)
+{
+  Guesser object("Secret");
+  if (guess.length()==2);{
+	if (distance(guess) >=2){
+		cout << "gives away >=2 length secrets; problem with unsigned int Guesser::distance(string guess)" << endl;
+		return True;
+
+	}
+  }
+  cout << "does not give away >=2 length secrets" << endl;
+  return False;
   cout << "remaining_tries: " << m_remaining << endl;
 }
